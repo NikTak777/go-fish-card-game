0837f2ca5f63fc18412874a7acdb6f2f66845490
NikTak777 <nikkondrakhin@gmail.com>
2026-06-19 20:11:01 +0300

    Add func to display user cards and count

--- GoFish/GoFish/GoFish.cpp
+++ GoFish/GoFish/GoFish.cpp
@@ -16,6 +16,8 @@ public:
     ~SetsOfCards() {
         delete[] deck_of_cards;
         delete[] count_of_cards;
+        delete[] player_cards;
+        delete[] opponent_cards;
     }
 
     void print() {
@@ -33,18 +35,24 @@ public:
         std::cout << std::endl;
     }
 
-    void get_player_card() {
+    void give_player_card() {
         signed char new_card = rand() % 13;
-        std::cout << (int)new_card << std::endl;
         player_cards[(int)new_card]++;
         count_of_cards[(int)new_card]--;
     }
 
-    void get_opponent_card() {
+    void give_opponent_card() {
         signed char new_card = rand() % 13;
-        std::cout << (int)new_card << std::endl;
         opponent_cards[(int)new_card]++;
         count_of_cards[(int)new_card]--;
+    }
+
+    signed char* get_player_suit_cards() {
+        return deck_of_cards;
+    }
+
+    signed char* get_player_count_cards() {
+        return player_cards;
     }
 };
 
@@ -59,11 +67,33 @@ public:
     void StartGame() {
         Cards.print();
         for (int i = 0; i < 7; ++i) {
-            Cards.get_player_card();
-            Cards.get_opponent_card();
+            Cards.give_player_card();
+            Cards.give_opponent_card();
         }
         Cards.print();
+        print_player_cards();
     }
+
+    void print_player_cards() {
+        for (int i = 0; i < 13; ++i) {
+            if ((int)Cards.get_player_count_cards()[i] > 0) {
+                std::cout << Cards.get_player_suit_cards()[i] << " ";
+            }
+        }
+        std::cout << std::endl;
+        for (int i = 0; i < 13; ++i) {
+            if ((int)Cards.get_player_count_cards()[i] > 0) {
+                std::cout << (int)Cards.get_player_count_cards()[i] << " ";
+            }
+        }
+        std::cout << std::endl;
+        for (int i = 0; i < 13; ++i) {
+            if (i == 1) std::cout << "^ ";
+            else std::cout << "  ";
+        }
+    }
+
+
 
 };
 
